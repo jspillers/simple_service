747e4e2694759d03482fd6ff900e02d5959d6707
Jrod <jarrod@jtms.net>
2015-04-03 16:01:34 -0400

    add travis ci and codeclimate test coverage integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib', 'simple_service')
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 $LOAD_PATH << File.join(File.dirname(__FILE__))
 
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 require 'pry'
 require 'rspec'
 require 'simple_service'
