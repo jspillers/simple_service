8c3412bf8ce306d013a45d3e7f64aaabb1617185
Jrod <jarrod@jtms.net>
2015-04-08 18:16:31 -0400

    update examples and fix organizer validation

--- lib/simple_service/commands/validates_commands_properly_inherit.rb
+++ lib/simple_service/commands/validates_commands_properly_inherit.rb
@@ -6,13 +6,15 @@ module SimpleService
     skip_validation true
 
     def execute
-      invalid_command_inherit = provided_commands.to_a.select do |command|
-        # does the command class inherit from SimpleService::Command
-        !(command.ancestors.include?(SimpleService::Command))
+      # valid commands inherit from Command and do not inherit from service
+      # reject all valid commands and anything left over is invalid
+      invalid_commands = provided_commands.to_a.reject do |command|
+        command.ancestors.include?(SimpleService::Command) ||
+        command.ancestors.include?(SimpleService::Organizer)
       end
 
-      if invalid_command_inherit.any?
-        error_msg = invalid_command_inherit.join(', ') +
+      if invalid_commands.any?
+        error_msg = invalid_commands.join(', ') +
           ' - must inherit from SimpleService::Command'
         raise SimpleService::CommandParentClassInvalidError, error_msg
       end
--- lib/simple_service/version.rb
+++ lib/simple_service/version.rb
@@ -1,3 +1,3 @@
 module SimpleService
-  VERSION = '1.1.0'
+  VERSION = '1.1.1'
 end
