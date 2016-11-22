367adeef3c4cded3a88ea46771277002bf7be5ba
Jrod <jarrod@jtms.net>
2016-11-22 11:11:18 -0500

    update code climate

--- simple_service.gemspec
+++ simple_service.gemspec
@@ -22,5 +22,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.4.2'
   spec.add_development_dependency 'rspec', '~> 3.2.0'
   spec.add_development_dependency 'pry', '~> 0.10.1'
+  spec.add_development_dependency 'simplecov'
   spec.add_development_dependency 'codeclimate-test-reporter'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib', 'simple_service')
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 $LOAD_PATH << File.join(File.dirname(__FILE__))
 
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 
 require 'pry'
 require 'rspec'
